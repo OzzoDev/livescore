feee1d19b45a5f8b41392a6456f4add81f0c9686
= <=>
2024-07-20 19:20:38 +0200

    Created the main api client with API football api

---
+++ src/services/api-client.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export interface FetchRespone<T>{
+    results: T[];    
+}
+
+const axiosInstance = axios.create({
+    baseURL: "https://api-football-v1.p.rapidapi.com/v3",
+    params:{
+        key: "371f3a3923mshd7c1b5aa7dce86ep17b1b0jsn9fcd770bb9ee",
+    },
+});
+
+class APIClient<T>{
+
+    endpoint: string;
+
+    constructor(endpoint: string){
+        this.endpoint = endpoint;
+    }
+
+    getAll = (config: AxiosRequestConfig)=>{
+        return axiosInstance
+        .get<FetchRespone<T>>(this.endpoint, config)
+        .then((res) => res.data);
+    }
+
+    get=(id:number|string)=>{
+        return axiosInstance
+        .get<T>(this.endpoint+"/"+id)
+        .then((res)=>res.data)
+    }
+
+}
